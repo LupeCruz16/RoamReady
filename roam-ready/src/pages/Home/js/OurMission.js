d564a29abe4e0e1ce42f0fcc25bd821b842c4929
Guadalupe Cruz <113732018+LupeCruz16@users.noreply.github.com>
2023-12-11 18:16:30 +0000

    Changing screen font sizes, media only invoked

--- roam-ready/src/pages/Home/js/OurMission.js
+++ roam-ready/src/pages/Home/js/OurMission.js
@@ -11,14 +11,15 @@ function OurMission () {
                                 <div className = "textSizeM textAlign-Left">Our Mission</div>
                             </div>
                             <div className = "missionContent-Right">
-                                <p className = "textSizeS marginLeft-S textAlign-Left">
+                                <div className = "textSizeS marginLeft-S textAlign-Left">
                                 Traveling is fun, however we run into the issue of where do we go too often. 
                                 If you are a thrill seeker who is so spontaneous you would allow our site to give 
                                 you your next travel location, then we have got you covered! 
                                 <br></br>
+                                <br></br>
                                 We are proud to help inspire you and give you our known tips and tricks for 
                                 traveling anywhere in the world!
-                                </p>
+                                </div>
                             </div>
                         </div>
                     </div>
